6bab9951193911b31991cb3f1ffd63b4fe75b5a7
Ashley Wright <ashleyelainewright@gmail.com>
2023-02-10 14:19:19 -0600

    lift off part 4

--- client/src/containers/track-card.js
+++ client/src/containers/track-card.js
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 import { colors, mq } from '../styles';
 import { humanReadableTimeFromSeconds } from '../utils/helpers';
 import { Link } from "@reach/router";
+import { gql, useMutation } from "@apollo/client";
 
 /**
  * Track Card component renders basic info in a card format
@@ -11,8 +12,33 @@ import { Link } from "@reach/router";
 const TrackCard = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track;
 
+  /**
+   * Mutation to increment a track's number of views
+   */
+  const INCREMENT_TRACK_VIEWS = gql`
+    mutation IncrementTrackViews($incrementTrackViewsId: ID!) {
+      incrementTrackViews(id: $incrementTrackViewsId) {
+        code
+        success
+        message
+        track {
+          id
+          numberOfViews
+        }
+      }
+    }
+  `;
+
+  const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: { incrementTrackViewsId: id },
+    // to observe what the mutation response returns
+    onCompleted: (data) => {
+      console.log(data);
+    },
+  });
+
   return (
-    <CardContainer to={`/track/${id}`}>
+    <CardContainer to={`/track/${id}`} onClick={incrementTrackViews}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail} alt={title} />
